3b982dfa39b9c5d0e1951ac876a4213efc3f2180
mohsen <smoka7@hotmail.com>
2023-11-08 18:39:23 +0330

    fix: Don't override existing highlight definition

--- lua/hop/highlight.lua
+++ lua/hop/highlight.lua
@@ -3,23 +3,25 @@ local M = {}
 
 -- Insert the highlights that Hop uses.
 function M.insert_highlights()
+  local set = vim.api.nvim_set_hl
+
   -- Highlight used for the mono-sequence keys (i.e. sequence of 1).
-  vim.api.nvim_set_hl(0, 'HopNextKey', { fg = '#ff007c', bold = true, ctermfg = 198, cterm = { bold = true } })
+  set(0, 'HopNextKey', { fg = '#ff007c', bold = true, ctermfg = 198, cterm = { bold = true }, default = true })
 
   -- Highlight used for the first key in a sequence.
-  vim.api.nvim_set_hl(0, 'HopNextKey1', { fg = '#00dfff', bold = true, ctermfg = 45, cterm = { bold = true } })
+  set(0, 'HopNextKey1', { fg = '#00dfff', bold = true, ctermfg = 45, cterm = { bold = true }, default = true })
 
   -- Highlight used for the second and remaining keys in a sequence.
-  vim.api.nvim_set_hl(0, 'HopNextKey2', { fg = '#2b8db3', ctermfg = 33 })
+  set(0, 'HopNextKey2', { fg = '#2b8db3', ctermfg = 33, default = true })
 
   -- Highlight used for the unmatched part of the buffer.
-  vim.api.nvim_set_hl(0, 'HopUnmatched', { fg = '#666666', sp = '#666666', ctermfg = 242 })
+  set(0, 'HopUnmatched', { fg = '#666666', sp = '#666666', ctermfg = 242, default = true })
 
   -- Highlight used for the fake cursor visible when hopping.
-  vim.api.nvim_set_hl(0, 'HopCursor', { link = 'Cursor' })
+  set(0, 'HopCursor', { link = 'Cursor', default = true })
 
   -- Highlight used for preview pattern
-  vim.api.nvim_set_hl(0, 'HopPreview', { link = 'IncSearch' })
+  set(0, 'HopPreview', { link = 'IncSearch', default = true })
 end
 
 function M.create_autocmd()
