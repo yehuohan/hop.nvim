42154e4e23146ad012ac0d43a87db223aeab6f67
yehuohan <yehuohan@qq.com>
2025-03-26 21:29:21 +0800

    fix(jumper.lua): stop move when window disappeared

--- lua/hop/jumper.lua
+++ lua/hop/jumper.lua
@@ -8,21 +8,23 @@ local api = vim.api
 --- Move cursor to jump target
 ---@param jump_target JumpTarget
 ---@param opts Options
+---@return boolean Moved or not
 function M.move_cursor(jump_target, opts)
     local jt = jump_target
 
+    if not vim.api.nvim_win_is_valid(jt.window) then
+        vim.notify(string.format('The window %d had disappeared', jt.window), vim.log.levels.ERROR)
+        return false
+    end
+    api.nvim_set_current_win(jt.window)
+
     -- If it is pending for operator shift cursor.col to the right by 1
-    if api.nvim_get_mode().mode == 'no' then
+    if fn.mode() == 'no' then
         jt.cursor.col = jt.cursor.col + 1
     end
 
     -- Update the jump list
     -- There is bug with set extmark neovim/neovim#17861
-    if vim.api.nvim_win_is_valid(jt.window) then
-        api.nvim_set_current_win(jt.window)
-    else
-        vim.notify(string.format('The window %d had disappeared', jt.window), vim.log.levels.ERROR)
-    end
     -- api.nvim_buf_set_mark(jt.buffer, "'", jt.cursor.row, jt.cursor.col + jt.cursor.off, {})
     vim.cmd("normal! m'")
 
@@ -32,15 +34,18 @@ function M.move_cursor(jump_target, opts)
     fn.setpos('.', { jt.buffer, jt.cursor.row, jt.cursor.col + 1, jt.cursor.off })
     fn.winrestview({ curswant = fn.virtcol('.') - 1 })
     require('hop').echo(string.format('Jump to [%s, %s, %s]', jt.cursor.row, jt.cursor.col + 1, jt.cursor.off), 'sel')
+
+    return true
 end
 
 --- Move multicursor to jump target
 ---@param jump_target JumpTarget
 ---@param opts Options
+---@return boolean Moved or not
 function M.move_multicursor(jump_target, opts)
     local mc = require('multicursor-nvim')
     if not mc.cursorsEnabled() then
-        M.move_cursor(jump_target, opts)
+        return M.move_cursor(jump_target, opts)
     else
         local jt_cur = jump_target.cursor
         local dpos = {}
@@ -54,7 +59,9 @@ function M.move_multicursor(jump_target, opts)
             end, { enabledCursors = true, disabledCursors = false })
         end)
 
-        M.move_cursor(jump_target, opts)
+        if not M.move_cursor(jump_target, opts) then
+            return false
+        end
 
         -- Add new cursors
         mc.clearCursors()
@@ -71,6 +78,8 @@ function M.move_multicursor(jump_target, opts)
             'sel'
         )
     end
+
+    return true
 end
 
 return M
